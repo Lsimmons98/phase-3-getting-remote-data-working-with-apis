a7714c91f9bcaae57c128ebc8741fd1fe8903f34
Liam Simmons <liam.simmons@powerhrg.com>
2024-07-23 10:50:31 -0400

    Add method to parse Json data and interate through it

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
- 
+
 class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
@@ -11,6 +11,14 @@ class GetPrograms
     response = Net::HTTP.get_response(uri)
     response.body
   end
+
+  def program_school
+      programs = JSON.parse(self.get_programs)
+      programs.collect do |program|
+        program["agency"]
+      end
+  end
+
 
 end
 
